57d970425bb4bed2d7741688cd3a7aef1327ebb5
Courtney McBeth <cmcbeth5287@gmail.com>
2026-03-10 12:44:56 -0500

    writing bounding box coordinates as output

--- run_demo.py
+++ run_demo.py
@@ -75,6 +75,18 @@ if __name__=='__main__':
       cv2.imshow('1', vis[...,::-1])
       cv2.waitKey(1)
 
+      # Transform 8 3D bbox corners into camera frame and save axis-aligned bounds
+      xmin, ymin, zmin = bbox[0]
+      xmax, ymax, zmax = bbox[1]
+      corners_3d = np.array([[x, y, z] for x in [xmin, xmax] for y in [ymin, ymax] for z in [zmin, zmax]])
+      corners_cam = (center_pose @ np.hstack([corners_3d, np.ones((8, 1))]).T).T[:, :3]
+      cam_min = corners_cam.min(axis=0)
+      cam_max = corners_cam.max(axis=0)
+      os.makedirs(f'{debug_dir}/bbox_cam', exist_ok=True)
+      np.savetxt(f'{debug_dir}/bbox_cam/{reader.id_strs[i]}.txt',
+                 np.stack([cam_min, cam_max]),
+                 header='rows: min max, cols: x y z (camera frame)', fmt='%.6f')
+
 
     if debug>=2:
       os.makedirs(f'{debug_dir}/track_vis', exist_ok=True)
